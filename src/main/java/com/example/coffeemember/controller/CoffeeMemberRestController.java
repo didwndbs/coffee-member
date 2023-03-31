1409f4561869e78871160f47c00efb78c0cbe48c
didwndbs <didwndbs@naver.com>
2023-03-31 11:56:23 +0900

    Initial commit

---
+++ src/main/java/com/example/coffeemember/controller/CoffeeMemberRestController.java
@@ -0,0 +1,16 @@
+package com.example.coffeemember.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/coffee-member")
+public class CoffeeMemberRestController {
+
+    @GetMapping("/")
+    public String coffeeMember(){
+        return "coffee-member service";
+    }
+}
